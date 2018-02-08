142c8be23d685fa9dc8d7c4a6c45a72b7d6cf71c
xesam <xesamguo@gmail.com>
2018-02-08 14:49:23 +0800

    'move'

--- baidu/move-marker.js
+++ baidu/move-marker.js
@@ -16,6 +16,7 @@ MoveMarker.prototype.moveTo = function (to, running_cbk, finish_cbk) {
         to,
         this.speed,
         this.fps,
+        null, 
         function (running, index) {
             var nextPixel = new BMap.Pixel(running.x, running.y);
             var nextPos = _projection.pointToLngLat(nextPixel);
@@ -91,40 +92,44 @@ MoveMarker.prototype.speed = function (newSpeed) {
     
 }
 
-
-function move_tick(from, to, speed, fps, running_cbk, finish_cbk) {
+function move_tick(from, to, speed, fps, first_cbk, tick_cbk, last_cbk) {
     var frame_duration = 1000 / fps;
     var delta_x = to.x - from.x;
     var delta_y = to.y - from.y;
     var total_distance = Math.sqrt(Math.pow(delta_x, 2) + Math.pow(delta_y, 2));
     var seconds = total_distance / speed;
-    var real_frames = seconds * fps;
-    var final_frames = Math.floor(real_frames);
-    var need_fill = real_frames > final_frames;
+    var actual_frames = seconds * fps;
+    var normal_frames = Math.floor(actual_frames);
+    var final_frames = Math.ceil(actual_frames);
+    var need_fill = final_frames > normal_frames;
     var step_x = delta_x / final_frames;
     var step_y = delta_y / final_frames;
 
     var frame_index = 0;
-    // console.log(total_distance, seconds, step_x, real_frames, final_frames, need_fill);
+    var current_pos = {
+        x: from.x,
+        y: from.y
+    };
+    first_cbk && first_cbk(current_pos, frame_index);
     var interval_flag = setInterval(function () {
-        if (frame_index >= final_frames) {
+        if (frame_index >= normal_frames) {
             clearInterval(interval_flag);
-            if (need_fill && finish_cbk) {
-                finish_cbk({
+            if (need_fill && last_cbk) {
+                last_cbk({
                     x: to.x,
                     y: to.y
                 }, frame_index);
             }
-            return;
+        }else{
+            current_pos.x += step_x;
+            current_pos.y += step_y;
+            var running = {
+                x : current_pos.x,
+                y : current_pos.y
+            };
+            tick_cbk && tick_cbk(running, frame_index);
+            frame_index++;
         }
-
-        var running = {};
-        running.x = from.x + frame_index * step_x;
-        running.y = from.y + frame_index * step_y;
-        if (running_cbk) {
-            running_cbk(running, frame_index);
-        }
-        frame_index++;
     }, frame_duration);
     return interval_flag;
 }
