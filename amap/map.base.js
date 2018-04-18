ba909206f29c12f113897b9c1b0e40dfcec738e6
xesam <xesamguo@gmail.com>
2018-04-19 00:12:29 +0800

    'demo'

--- amap/map.base.js
+++ amap/map.base.js
@@ -2,8 +2,8 @@ var Map = {
   init: function(container, cbk) {
     var map = new AMap.Map(container, {
       resizeEnable: true,
-      zoom: 13,
-      center: [113.159649, 22.995836],
+      zoom: 14,
+      center: [113.220007, 22.879622],
       mapStyle: "amap://styles/aaadd24c5d8483a20ded5cea742dcecc"
     });
     cbk && cbk(map);
