2cbf8e2f0ef82c21e9802a10f30a74e92b381caf
xesam <xesamguo@gmail.com>
2018-05-23 21:28:42 +0800

    'play'

--- amap/map.demo.js
+++ amap/map.demo.js
@@ -1,12 +1,12 @@
 var Map = {
-  init: function(container, cbk) {
-    var map = new AMap.Map(container, {
-      resizeEnable: true,
-      zoom: 14,
-      center: [113.20799, 22.879587],
-      mapStyle: "amap://styles/aaadd24c5d8483a20ded5cea742dcecc"
-    });
-    cbk && cbk(map);
-    return map;
-  }
+    init: function (container, cbk) {
+        var map = new AMap.Map(container, {
+            resizeEnable: true,
+            zoom: 14,
+            center: [117.259588, 39.0537],
+            mapStyle: "amap://styles/aaadd24c5d8483a20ded5cea742dcecc"
+        });
+        cbk && cbk(map);
+        return map;
+    }
 };
